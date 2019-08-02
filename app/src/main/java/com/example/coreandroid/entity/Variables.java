afeda8dc8f0fb1b20f0a91a830410effad157970
ApilPokhrel <40927803+ApilPokhrel@users.noreply.github.com>
2019-08-02 09:29:12 +0545

    Update Variables.java

--- app/src/main/java/com/example/coreandroid/entity/Variables.java
+++ app/src/main/java/com/example/coreandroid/entity/Variables.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Variables implements Serializable {
 
-    public static  final String MAP_KEY = "AIzaSyDNBX0f7EXUVB5lBUx7vGQ66l7YvnZx8tY";
+    public static  final String MAP_KEY = "GOOGLE_MAP_API_KEY";
     public static final String BASE_URL = "https://c6a9f240.ngrok.io";
     public static final int ERROR_DIALOG_REQUEST = 9001;
     public static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
